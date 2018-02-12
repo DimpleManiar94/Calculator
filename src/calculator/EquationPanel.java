487fdeff3b44f789cda34ef20ae7398536fcc829
shaishavgandhi05 <shaishgandhi@gmail.com>
2018-02-12 15:07:24 -0800

    Decomponse code
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

---
+++ src/calculator/EquationPanel.java
@@ -0,0 +1,25 @@
+package calculator;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JList;
+import javax.swing.JScrollBar;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+
+public class EquationPanel extends JPanel {
+	
+	public EquationPanel() {
+		addKeys();
+	}
+	
+	public void addKeys() {
+		
+	}
+
+}
